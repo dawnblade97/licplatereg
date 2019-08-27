0d5c33a327c5231825b6442bd381a937093a6088
dawnblade97 <49042994+dawnblade97@users.noreply.github.com>
2019-08-27 09:17:07 +0530

    added code to detect LP from video

---
+++ video_run.sh
@@ -0,0 +1,4 @@
+echo "..............................................video detection............................................."
+echo "enter the name of the video file must be in the same directory"
+read n
+python darknet_video.py $n
